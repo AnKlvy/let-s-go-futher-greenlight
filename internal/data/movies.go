f60461dcf6d31742aee1332d1c57025524e1d053
Andrey Klimov <andreyklimov.ver2@gmail.com>
2025-02-23 11:15:02 +0500

    made an envelope for json to make structure for data

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -15,6 +15,16 @@ type Movie struct {
 	// time the movie information is updated
 }
 
+// type Movie struct {
+// 	ID int64 `json:"id"`
+// 	CreatedAt time.Time `json:"-"` // Use the - directive
+// 	Title string `json:"title"`
+// 	Year int32 `json:"year,omitempty"` // Add the omitempty directive
+// 	Runtime int32 `json:"runtime,omitempty"` // Add the omitempty directive
+// 	Genres []string `json:"genres,omitempty"` // Add the omitempty directive
+// 	Version int32 `json:"version"`
+// 	}
+
 //Important: It’s crucial to point out here that all the fields in our Movie struct are
 // exported (i.e. start with a capital letter), which is necessary for them to be visible to
 // Go’s encoding/json package. Any fields which aren’t exported won’t be included
