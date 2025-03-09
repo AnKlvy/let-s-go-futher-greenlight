8857a6dd1be151623451f7bfb03d6cf5a9c2c111
Andrey Klimov <andreyklimov.ver2@gmail.com>
2025-03-09 22:40:59 +0500

    added timeout for crud requests

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -1,6 +1,7 @@
 package data
 
 import (
+	"context"
 	"database/sql"
 	"errors"
 	"github.com/lib/pq"
@@ -13,43 +14,38 @@ type MovieModel struct {
 	DB *sql.DB
 }
 
-// Метод Insert() принимает указатель на структуру Movie, которая должна содержать
-// данные для новой записи.
 func (m MovieModel) Insert(movie *Movie) error {
-	// Определяем SQL-запрос для вставки новой записи в таблицу movies и получения
-	// автоматически сгенерированных данных.
 	query := `
-	INSERT INTO movies (title, year, runtime, genres)
-	VALUES ($1, $2, $3, $4)
-	RETURNING id, created_at, version`
-	// Создаем срез args, содержащий значения для параметров-заполнителей из
-	// структуры movie. Объявление этого среза сразу рядом с нашим SQL-запросом помогает
-	// сделать более понятным *какие значения используются и где* в запросе.
+    INSERT INTO movies (title, year, runtime, genres)
+    VALUES ($1, $2, $3, $4)
+    RETURNING id, created_at, version`
 	args := []any{movie.Title, movie.Year, movie.Runtime, pq.Array(movie.Genres)}
-	// Используем метод QueryRow() для выполнения SQL-запроса с нашим пулом соединений,
-	// передавая срез args как вариативный параметр и сканируем значения id, created_at и version
-	// в структуру movie.
-	return m.DB.QueryRow(query, args...).Scan(&movie.ID, &movie.CreatedAt, &movie.Version)
+
+	// Создаём контекст с тайм-аутом 3 секунды.
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	// Используем QueryRowContext() и передаём контекст в качестве первого аргумента.
+	return m.DB.QueryRowContext(ctx, query, args...).Scan(&movie.ID, &movie.CreatedAt, &movie.Version)
 }
 
 func (m MovieModel) Get(id int64) (*Movie, error) {
-	// Тип bigserial в PostgreSQL начинает автоинкрементацию с 1 по умолчанию,
-	// поэтому мы знаем, что фильмы не могут иметь ID меньше 1.
-	// Чтобы избежать ненужного запроса к базе данных, сразу возвращаем ошибку ErrRecordNotFound.
 	if id < 1 {
 		return nil, ErrRecordNotFound
 	}
-	// Определяем SQL-запрос для получения данных о фильме.
+
+	// Удаляем конструкцию pg_sleep(10).
 	query := `
-	SELECT id, created_at, title, year, runtime, genres, version
-	FROM movies
-	WHERE id = $1`
-	// Объявляем структуру Movie для хранения данных, полученных из запроса.
+    SELECT id, created_at, title, year, runtime, genres, version
+    FROM movies
+    WHERE id = $1`
+
 	var movie Movie
-	// Выполняем запрос с помощью метода QueryRow(), передавая значение id в качестве параметра.
-	// Затем используем Scan() для записи данных в структуру Movie.
-	// Важно: для преобразования данных столбца genres снова используем адаптер pq.Array().
-	err := m.DB.QueryRow(query, id).Scan(
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	// Убираем &[]byte{} из первого аргумента Scan().
+	err := m.DB.QueryRowContext(ctx, query, id).Scan(
 		&movie.ID,
 		&movie.CreatedAt,
 		&movie.Title,
@@ -58,8 +54,6 @@ func (m MovieModel) Get(id int64) (*Movie, error) {
 		pq.Array(&movie.Genres),
 		&movie.Version,
 	)
-	// Обрабатываем возможные ошибки. Если фильм не найден, Scan() вернет ошибку sql.ErrNoRows.
-	// В этом случае возвращаем нашу кастомную ошибку ErrRecordNotFound.
 	if err != nil {
 		switch {
 		case errors.Is(err, sql.ErrNoRows):
@@ -68,29 +62,30 @@ func (m MovieModel) Get(id int64) (*Movie, error) {
 			return nil, err
 		}
 	}
-	// Если ошибок нет, возвращаем указатель на структуру Movie.
 	return &movie, nil
 }
 
 func (m MovieModel) Update(movie *Movie) error {
-	// Добавляем условие 'AND version = $6' в SQL-запрос.
 	query := `
-	UPDATE movies
-	SET title = $1, year = $2, runtime = $3, genres = $4, version = version + 1
-	WHERE id = $5 AND version = $6
-	RETURNING version`
+    UPDATE movies
+    SET title = $1, year = $2, runtime = $3, genres = $4, version = version + 1
+    WHERE id = $5 AND version = $6
+    RETURNING version`
 	args := []any{
 		movie.Title,
 		movie.Year,
 		movie.Runtime,
 		pq.Array(movie.Genres),
 		movie.ID,
-		movie.Version, // Добавляем ожидаемую версию фильма.
+		movie.Version,
 	}
-	// Выполняем SQL-запрос. Если соответствующая строка не найдена, это означает,
-	// что версия фильма изменилась (или запись была удалена), и мы возвращаем
-	// нашу пользовательскую ошибку ErrEditConflict.
-	err := m.DB.QueryRow(query, args...).Scan(&movie.Version)
+
+	// Создаём контекст с тайм-аутом 3 секунды.
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	// Используем QueryRowContext() и передаём контекст в качестве первого аргумента.
+	err := m.DB.QueryRowContext(ctx, query, args...).Scan(&movie.Version)
 	if err != nil {
 		switch {
 		case errors.Is(err, sql.ErrNoRows):
@@ -103,36 +98,29 @@ func (m MovieModel) Update(movie *Movie) error {
 }
 
 func (m MovieModel) Delete(id int64) error {
-	// Возвращаем ошибку ErrRecordNotFound, если ID фильма меньше 1.
 	if id < 1 {
 		return ErrRecordNotFound
 	}
-
-	// Формируем SQL-запрос для удаления записи.
 	query := `
-		DELETE FROM movies
-		WHERE id = $1`
+    DELETE FROM movies
+    WHERE id = $1`
 
-	// Выполняем SQL-запрос с помощью метода Exec(), передавая переменную id
-	// в качестве значения для плейсхолдера. Метод Exec() возвращает объект sql.Result.
-	result, err := m.DB.Exec(query, id)
+	// Создаём контекст с тайм-аутом 3 секунды.
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	// Используем ExecContext() и передаём контекст в качестве первого аргумента.
+	result, err := m.DB.ExecContext(ctx, query, id)
 	if err != nil {
 		return err
 	}
-
-	// Вызываем метод RowsAffected() у объекта sql.Result, чтобы получить количество
-	// строк, затронутых запросом.
 	rowsAffected, err := result.RowsAffected()
 	if err != nil {
 		return err
 	}
-
-	// Если ни одна строка не была затронута, значит, в таблице movies не было записи
-	// с переданным ID на момент выполнения удаления. В этом случае возвращаем ошибку ErrRecordNotFound.
 	if rowsAffected == 0 {
 		return ErrRecordNotFound
 	}
-
 	return nil
 }
 
